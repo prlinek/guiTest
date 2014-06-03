afc03cac04dbe0c81bec7a0f5889e0bc8776d81c
prlinek <prlinek@gmail.com>
2014-06-03 10:49:17 +0100

    GUI v0.2
    Plot canvas is embeded in program window. Plot title and labels not
    working.

--- gui.py
+++ gui.py
@@ -12,25 +12,36 @@ class TkDialog(Tkinter.Frame):
     def __init__(self, root):
         Tkinter.Frame.__init__(self, root)
         # options for a button
-        button_opt = {'fill': Tkconstants.BOTH, 'padx': 5, 'pady': 1}
-        radio_opt = {'fill': Tkconstants.BOTH, 'padx': 5, 'pady': 1}
-        # defining buttons
-        Tkinter.Button(self, text='Load Data', command=self.askopenfilename).pack(**button_opt)
-        Tkinter.Button(self, text='Plot', command=self.showplot).pack(**button_opt)
-        # defining variable for radio buttons
+        button_opt = {'fill': Tkconstants.BOTH, 'padx': 5, 'pady': 1, 'side': Tkconstants.LEFT}
+        radio_opt = {'fill': Tkconstants.Y, 'padx': 5, 'pady': 1, 'side': Tkconstants.LEFT}
+         # defining variable for radio buttons
         self.radio_selection = Tkinter.IntVar()
         # defining radio buttons
-        Tkinter.Radiobutton(self, text='Single file', variable=self.radio_selection, value=1).pack(**radio_opt)
+        Tkinter.Radiobutton(self, text='Single file   ', variable=self.radio_selection, value=1).pack(**radio_opt)
         Tkinter.Radiobutton(self, text='Multiple file', variable=self.radio_selection, value=2).pack(**radio_opt)
+        # defining buttons
+        Tkinter.Button(self, text='Load Data', command=self.askopenfilename).pack(**button_opt)
+        Tkinter.Button(self, text='Plot Data', command=self.showplot).pack(**button_opt)
 
-        # fig = plt.figure()
-        # canvas = FigureCanvasTkAgg(fig)
-        # toolbar = NavigationToolbar2TkAgg(canvas)
-        # canvas.get_tk_widget()
+        # canvas = Tkinter.Canvas(root, width=400, height=400, bg='white').pack()
+        # NavigationToolbar2TkAgg(canvas).pack()
+
+        # f = plt.Figure(figsize=(5, 4), dpi=100)
+        # dataPlot = FigureCanvasTkAgg(f, master=root)
+        # dataPlot.show()
+        # dataPlot.get_tk_widget().pack(side=Tkconstants.BOTTOM, fill=Tkconstants.BOTH, expand=1)
+        self.f = plt.Figure()
+        self.canvas = FigureCanvasTkAgg(self.f, master=root)
+        self.canvas.get_tk_widget().pack(side=Tkconstants.BOTTOM, fill=Tkconstants.BOTH, expand=1)
+        self.canvas.show()
+
+        self.toolbar = NavigationToolbar2TkAgg(self.canvas, root )
+        self.toolbar.pack(side=Tkconstants.BOTTOM, fill=Tkconstants.BOTH, expand=1)
+        self.toolbar.update()
 
         #define options for opening files
         self.file_opt = options = {}
-        # options['initialdir'] = 'C:/'
+        options['initialdir'] = '../DataPack1'
         options['parent'] = root
         options['title'] = 'Title'
 
@@ -51,15 +62,15 @@ class TkDialog(Tkinter.Frame):
         if self.radio_selection.get() == 1:
             magnitude = rd.data[:, 1]
             wavelength = rd.data[:, 0]
-            # plt.clf()
-            plt.plot(wavelength, magnitude)
-            plt.title("Plot from single file")
-            plt.ylabel("magnitude")
-            plt.xlabel("wavelength")
-            plt.grid()
-            # plt.draw()
-            # plt.gcf().canvas.draw()
-            plt.show()
+            self.f.clear()
+            self.fig = self.f.add_subplot(111)
+            self.fig.plot(wavelength, magnitude)
+            # self.fig.title("Plot from single file")
+            # self.fig.ylabel("magnitude")
+            # self.fig.xlabel("wavelength")
+            self.fig.grid()
+            self.canvas.draw()
+            # plt.show()
         elif self.radio_selection.get() == 2:
             # for now it shows plot from first chosen data file
             magnitude = rd.x[0][:, 1]
