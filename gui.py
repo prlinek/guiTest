fdc8370c66558f058572e1724df6f0324a0660b0
prlinek <prlinek@gmail.com>
2014-06-08 22:32:33 +0100

    GUI v0.5.2
    Playplot temporarily disabled, caused program to crash.

--- gui.py
+++ gui.py
@@ -6,6 +6,7 @@ import tkFileDialog
 import readdata as rd
 import matplotlib.pyplot as plt  # temporarly here, plot functions will be moved to separate file
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+# import time
 
 
 class TkDialog(Tkinter.Frame):
@@ -21,16 +22,30 @@ class TkDialog(Tkinter.Frame):
         # defining variable for radio buttons
         self.radio_selection = Tkinter.IntVar()
         self.radio_selection.set(1)
-        self.list_of_files = Tkinter.StringVar()
+        self.list_of_files = []
         # defining radio buttons
         Tkinter.Radiobutton(self, text='Single file   ', variable=self.radio_selection, value=1).pack(**radio_opt)
         Tkinter.Radiobutton(self, text='Multiple file', variable=self.radio_selection, value=2).pack(**radio_opt)
         # defining buttons
         Tkinter.Button(self, text='Load Data', command=self.askopenfilename).pack(**button_opt)
-        Tkinter.Button(self, text='Next Data', command=self.nextplot).pack(**button_opt)
-        Tkinter.Button(self, text='Previous Data', command=self.prevplot).pack(**button_opt)
+        self.nextbtn = Tkinter.Button(self, text='Next Data', command=self.nextplot)
+        self.nextbtn.pack(**button_opt)
+        self.prevbtn = Tkinter.Button(self, text='Previous Data', command=self.prevplot)
+        self.prevbtn.pack(**button_opt)
 
-        Tkinter.Button(self, text='Play', command=self.playplot).pack(**button_opt)
+        self.playcheck = Tkinter.IntVar(0)
+        # self.checkbut = Tkinter.Checkbutton(self, text='Play/Pause', command=self.playplot, variable=self.playcheck, onvalue=1, offvalue=0).pack()
+        self.playbtn = Tkinter.Button(self, text='Play', command=self.playplot)
+        self.playbtn.pack(**button_opt)
+        # playbtn.config(state=Tkconstants.DISABLED)
+        self.pausebtn = Tkinter.Button(self, text='Pause', command=self.pauseplot)
+        self.pausebtn.pack(**button_opt)
+
+        if self.radio_selection.get() == 1:
+            self.nextbtn.config(state=Tkconstants.DISABLED)
+            self.prevbtn.config(state=Tkconstants.DISABLED)
+            self.playbtn.config(state=Tkconstants.DISABLED)
+            self.pausebtn.config(state=Tkconstants.DISABLED)
         # creating new frame for containing plot and toolbar
         frame = Tkinter.Frame(root)
         frame.pack(side=Tkconstants.RIGHT)
@@ -55,16 +70,22 @@ class TkDialog(Tkinter.Frame):
             if filename:
                 rd.readFile(filename)
                 self.showplot()
+                self.nextbtn.config(state=Tkconstants.DISABLED)
+                self.prevbtn.config(state=Tkconstants.DISABLED)
+                self.playbtn.config(state=Tkconstants.DISABLED)
+                self.pausebtn.config(state=Tkconstants.DISABLED)
                 return rd.data
         elif self.radio_selection.get() == 2:
             self.list_of_files = tkFileDialog.askopenfilenames(**self.file_opt)
             self.list_of_files = self.list_of_files.split()
-            # print len(self.list_of_files)  # just for test
             rd.readFiles(self.list_of_files)
             if self.list_of_files:
                 self.plotnum.set(0)
-                self.vals = [0, len(self.list_of_files)]
                 self.showplot()
+                self.nextbtn.config(state=Tkconstants.NORMAL)
+                self.prevbtn.config(state=Tkconstants.NORMAL)
+                # self.playbtn.config(state=Tkconstants.NORMAL)
+                # self.pausebtn.config(state=Tkconstants.NORMAL)
             return rd.x
 
     def showplot(self):
@@ -102,8 +123,30 @@ class TkDialog(Tkinter.Frame):
         self.showplot()
 
     def playplot(self):
-        for n in self.list_of_files:
-            self.nextplot()
+        # # if self.playcheck.get() == 1:
+        # # self.playcheck.set(0)
+        # self.playbtn.config(state=Tkconstants.DISABLED)
+        # self.nextbtn.config(state=Tkconstants.DISABLED)
+        # self.prevbtn.config(state=Tkconstants.DISABLED)
+        # if len(self.list_of_files) != 0:
+        #     for n in self.list_of_files:
+        #         self.nextplot()
+        #         if self.playcheck.get() == 1:
+        #             break
+        #         # else:
+        #         #     continue
+        #         # time.sleep(0.5)
+        #     self.playbtn.config(state=Tkconstants.NORMAL)
+        #     self.nextbtn.config(state=Tkconstants.NORMAL)
+        #     self.prevbtn.config(state=Tkconstants.NORMAL)
+        # else:
+        pass
+
+    def pauseplot(self):
+        pass
+        # self.playcheck.set(1)
+        # print self.playcheck.get()
+
 
 if __name__ == '__main__':
     root = Tkinter.Tk()
